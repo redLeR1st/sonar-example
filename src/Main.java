df46f27d034ec7d87b8c751177d4dce6e05364bf
bartab <bartab@inf.u-szeged.hu>
2019-04-15 19:04:39 +0200

    Comm

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("commit");
         System.out.println("commit2");
+        System.out.println("valtozik");
 		
     }
 
